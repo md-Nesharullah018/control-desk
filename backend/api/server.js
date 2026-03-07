93627f44f3b639ba1f33a4d0eb092ffee5d83113
md nesharullah <mneshar018@gmail.com>
2026-03-08 01:25:35 +0530

    file settings

---
+++ backend/api/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import ServerlessHttp from "serverless-http";
+import { config } from "dotenv";
+import { connectDB } from "../config/db.js";
+import authRoutes from "../routes/authRoutes.js";
+
+config();
+
+const app = express();
+
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/auth_app";
+connectDB(mongoURI);
+
+
+app.use(cors()); 
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("Backend is running perfectly! 🚀");
+});
+
+
+
+export default app;
+export const handler = serverless(app);
+  
+// const PORT = process.env.PORT || 5000; 
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
